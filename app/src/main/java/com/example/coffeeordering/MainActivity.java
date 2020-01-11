894bbfae59d4ef02574e52296d1d51b1df579ff3
nehabalaji <nehabalaji10@gmail.com>
2020-01-12 02:10:25 +0530

    initial commit

--- app/src/main/java/com/example/coffeeordering/MainActivity.java
+++ app/src/main/java/com/example/coffeeordering/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.coffeeordering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText ed1, ed2;
+    private EditText ed1, ed2, ed3;
     private TextView tv1, tv2, tv3, tv4;
-    private int a=0, b=0, c=0, d=0;
-    private String name;
+    private int a=0, b=0, c=0, d=0, price, q=0, r=0, p=0;
+    private String name, email;
+    private CheckBox c1, c2, c3;
+    boolean e, f, g;
 
 
 
@@ -21,10 +25,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ed1 = findViewById(R.id.name_et);
         ed2 = findViewById(R.id.number_et);
+        ed3 = findViewById(R.id.email_et);
         tv1 = findViewById(R.id.count1);
         tv2 = findViewById(R.id.count2);
         tv3 = findViewById(R.id.count3);
         tv4 = findViewById(R.id.summary);
+        c1 =  findViewById(R.id.whipped1);
+        c2 = findViewById(R.id.whipped2);
+        c3 = findViewById(R.id.whipped3);
 
 
     }
@@ -60,13 +68,40 @@ public class MainActivity extends AppCompatActivity {
         tv3.setText(""+c);
     }
 
+
     public void submit(View view){
         d=a*20+b*20+c*20;
-        name=ed1.getText().toString();
-        name=name+" your bill amount is "+d+" rupees";
-        tv4.setText(""+name);
 
+        e = c1.isChecked();
+        f= c2.isChecked();
+        g= c3.isChecked();
+
+        if(e==true){
+            p=10;
+        }
+        if(f==true){
+            q=10;
+        }
+        if(g==true){
+            r=10;
+        }
+
+        price=d+p+q+r;
+
+        name=ed1.getText().toString();
+
+       tv4.setText(name+" your total amount is "+ price);
+       email=ed3.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Bill Summary");
+        intent.putExtra(Intent.EXTRA_TEXT, name+" your total amount is "+price);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
+
 
 
 }
